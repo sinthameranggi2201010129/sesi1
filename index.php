fe23364e90c7ea989023d4c65abe3815b856152e
sinthameranggi2201010129 <127836217+sinthameranggi2201010129@users.noreply.github.com>
2023-03-14 16:54:42 +0800

    sesi1

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan HTML Sesi 1</title>
+</head>
+<body>
+    
+    <h3> hallo world!<?="<sup>80</sup>"?> </h3>
+
+    <script>
+        document.write("<h3> apa mn aku ketik ya</h3>");
+    </script>
+
+<?php
+    echo "<h3> hallo </h3>" ;
+?>
+
+</body>
+</html>
